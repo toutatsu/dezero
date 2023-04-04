5ecd62b7dd46aa3c4e7cdd0dd804744608f0e9c2
toutatsu <toutatsu.00223+git@gmail.com>
2023-04-04 08:58:53 +0000

    backpropagation (recursion)

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -5,6 +5,17 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator # 1. 関数を取得
+        if f is not None:
+            x = f.input # 2. 関数の入力を取得
+            x.grad = f.backward(self.grad) # 3. 関数のbackwardメソッドを呼ぶ
+            x.backward() # 4. 自分より1つ前の変数のbackwardメソッドを呼ぶ (再帰)
 
 
 class Function:
@@ -12,7 +23,9 @@ class Function:
         x = input.data # データを取り出す
         y = self.forward(x) # 実際の計算
         output = Variable(y)
+        output.set_creator(self) # 出力変数に生みの親を覚えさせる
         self.input = input # 入力された変数を覚える
+        self.output = output # 出力も覚える
         return output
 
     def forward(self, x):
