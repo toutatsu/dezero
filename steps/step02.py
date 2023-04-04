7683c6bc072afe97c6cfee4f8c33ff998a5d8033
toutatsu <toutatsu.00223+git@gmail.com>
2023-04-04 07:10:07 +0000

    define class Function

--- steps/step02.py
+++ steps/step02.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-
 class Variable:
     def __init__(self, data):
         self.data = data
-
 
 class Function:
     def __call__(self, input):
@@ -15,3 +13,16 @@ class Function:
 
     def forward(self, x):
         raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+x = Variable(np.array(10))
+f = Square()
+y = f(x)
+
+
+print(type(y))
+print(y.data)
