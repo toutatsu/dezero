493d97108e4e6bf64c0cc65ddae5a66ea19c100f
toutatsu <toutatsu.00223+github@gmail.com>
2023-04-06 07:38:44 +0000

    overload operator + *

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -180,3 +180,21 @@ class Add(Function):
 
 def add(x0, x1):
     return Add()(x0, x1)
+
+
+class Mul(Function):
+
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return x1 * gy, x0 * gy
+
+def mul(x0, x1):
+    return Mul()(x0, x1)
+
+
+Variable.__add__ = add
+Variable.__mul__ = mul
