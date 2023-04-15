2b9ff056ca9ff70b6e5890046d29df01bf720cbb
toutatsu <toutatsu.00223+github@gmail.com>
2023-04-15 07:07:26 +0000

    add Function Sum

--- dezero/functions.py
+++ dezero/functions.py
@@ -108,3 +108,24 @@ class Transpose(Function):
 
 def transpose(x, axes=None):
     return Transpose(axes)(x)
+
+
+
+class Sum(Function):
+
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+    
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
