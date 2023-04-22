3d16704cbc93968bffd29f06f623d694c8b208bf
toutatsu <toutatsu.00223+github@gmail.com>
2023-04-22 13:59:55 +0000

    import modules

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -13,15 +13,24 @@ if is_simple_core:
 
 else:
     from dezero.core import Variable
+    from dezero.core import Parameter
     from dezero.core import Function
     from dezero.core import using_config
     from dezero.core import no_grad
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
+    from dezero.core import Config
     from dezero.layers import Layer
-    from dezero.models import Model 
+    from dezero.models import Model
 
     from dezero.functions import matmul
+
+    import dezero.datasets
+    import dezero.optimizers
+    import dezero.functions
+    import dezero.layers
+    import dezero.utils
+
 
 setup_variable()
