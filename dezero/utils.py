c376a4c1544f89286c14b331a5b7c7ca1ad5d28d
toutatsu <toutatsu.00223+github@gmail.com>
2023-04-28 06:38:27 +0000

    add convolution utilities

--- dezero/utils.py
+++ dezero/utils.py
@@ -258,5 +258,19 @@ def get_file(url, file_name=None):
 # =============================================================================
 # others
 # =============================================================================
+def get_deconv_outsize(size, k, s, p):
+    return s * (size - 1) + k - 2 * p
+
+
 def get_conv_outsize(input_size, kernel_size, stride, pad):
     return (input_size + pad * 2 - kernel_size) // stride + 1
+
+
+def pair(x):
+    if isinstance(x, int):
+        return (x, x)
+    elif isinstance(x, tuple):
+        assert len(x) == 2
+        return x
+    else:
+        raise ValueError
